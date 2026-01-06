5fbe68f4db8c17ad0c48c4619e47d2ae3523dce3
AntonioCortes <anntoniocorteslopez@gmail.com>
2026-01-06 20:23:16 +0100

    initial sql information

---
+++ js/components-imports.js
@@ -0,0 +1,7 @@
+// ===== SQL VIEW =====
+import '../components/sql-view/dql/dql.js';
+import '../components/sql-view/dml/dml.js';
+import '../components/sql-view/ddl/ddl.js';
+import '../components/sql-view/dcl/dcl.js';
+import '../components/sql-view/tcl/tcl.js';
+
